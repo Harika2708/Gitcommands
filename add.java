bcc013c41ee7e9dbb13e9ca33b0734d10ffab816
Sulaganti, Harika <I38935@verisk.com>
2024-11-26 14:25:38 +0530

    sumoftwonumbers

---
+++ add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class add{
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+       int a=5;
+       int b=3;
+       int c=a+b;
+       System.out.println(c);
+    }
+}
